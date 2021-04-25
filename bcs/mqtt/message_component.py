3ca52cec952f56aaadfea58e5f54e86755704891
MartinKliemank <martin.kliemank@gmx.de>
2021-04-25 18:13:44 +0200

    added destructor that deletes message file; play function that sets new to false and returns audio_file_path

--- bcs/mqtt/message_component.py
+++ bcs/mqtt/message_component.py
@@ -1,10 +1,16 @@
-
+import os
 
 class Message:
     def __init__(self, channel_name, ID, audio_file_path):
-        self.audio_file = audio_file_path
+        self.audio_file_path = audio_file_path
         self.channel_name = channel_name
         self.new = True
         self.ID = ID
 
-    #TODO: destructor that deletes message file
+    # destructor that deletes message file when message object is deleted
+    def __del__(self):
+        os.remove(self.audio_file_path)
+
+    def play(self):
+        self.new = False
+        return self.audio_file_path
